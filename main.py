bc9d5decd5e3bc4d59e6cc772b881d61ab3446ba
Marc <RhinoDevel@users.noreply.github.com>
2017-05-06 17:26:55 +0200

    str namespace.

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 
 import db
-
-def str_is_nonwhitespace(s):
-    """Is given argument a string that is neither empty, nor whitespace-only?
-    """
-
-    return isinstance(s, str) and s and not s.isspace()
-
-def str_read_file(path):
-    """Return content of file at given path as string.
-    """
-
-    with open(path, 'r') as f:
-        return f.read()
+import str
 
 def test():
     #db.write("UPDATE Task SET Title='01a' WHERE Nr = 11;")
@@ -23,6 +11,6 @@ test()
 test()
 db.close()
 
-dbInit = str_read_file('dbinit.sql')
+dbInit = str.read_file('dbinit.sql')
 
 print(dbInit)
---
+++ str/__init__.py
@@ -0,0 +1,13 @@
+
+def is_nonwhitespace(s):
+    """Is given argument a string that is neither empty, nor whitespace-only?
+    """
+
+    return isinstance(s, str) and s and not s.isspace()
+
+def read_file(path):
+    """Return content of file at given path as string.
+    """
+
+    with open(path, 'r') as f:
+        return f.read()
