5110ebf96bea7043e997c8612030c9c2c9bfb9d8
Marc <RhinoDevel@users.noreply.github.com>
2017-05-06 21:08:58 +0200

    Remove debug code.

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 
-import mt.db
-import mt.str
-
-def test():
-    #mt.db.write("UPDATE Task SET Title='01a' WHERE Nr = 11;")
-    print(mt.db.read_all("SELECT * FROM Task;"))
-    #close_conn()
-#
-test()
-test()
-mt.db.close()
-
-dbInit = mt.str.read_file('dbinit.sql')
-
-print(dbInit)
+print("Nothing implemented, yet..")
