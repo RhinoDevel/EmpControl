538b8568f3105d27f3dbf99bd215244493a3973b
Marc <RhinoDevel@users.noreply.github.com>
2017-05-23 22:08:12 +0200

    Add workday creation and printing.

--- ec/ui/console/workday.py
+++ ec/ui/console/workday.py
@@ -1,5 +1,7 @@
 
 import collections
+from datetime import datetime
+from datetime import timedelta
 import ec.db.workday
 import ec.ui.console
 
@@ -9,19 +11,45 @@ def _print():
         print('No entries.')
         return
 
-    print('ID        ' + ' | ' + 'Worker ID ' + ' | ' + 'Begin at  ' + ' | ' + 'End at    ' + ' | ' + 'Break     ' + ' | ' + 'Vacation  ' + ' | ' + 'Sick      ')
-    print('----------' + '---' + '----------' + '---' + '----------' + '---' + '----------' + '---' + '----------' + '---' + '----------' + '---' + '----------')
+    print('ID      ' + ' | ' + 'Worker ID' + ' | ' + 'Begin at                 ' + ' | ' + 'End at                   ' + ' | ' + 'Break' + ', ' + 'Vacation' + ', ' + 'Sick ')
+    print('--------' + '---' + '---------' + '---' + '-------------------------' + '---' + '-------------------------' + '---')
     for v in l: # TODO: Fix end strings!
-        print(v['id'], end='   | ')
-        print(v['worker_id'], end='   | ')
-        print(str(v['begin_at']), end='   | ')
-        print(str(v['end_at']), end='   | ')
-        print(str(v['break']), end='   | ')
-        print(str(v['vacation']), end=' | ')
+        print(v['id'], end=' | ')
+        print(v['worker_id'], end='  | ')
+        print(str(v['begin_at']), end=' | ')
+        print(str(v['end_at']), end=' | ')
+        print(str(v['break']), end=', ')
+        print(str(v['vacation']), end=', ')
         print(v['sick'])
 
+def _convert(data):
+    data['begin_at'] = datetime.strptime(data['begin_at'], '%Y-%m-%d %H:%M')
+    data['end_at'] = datetime.strptime(data['end_at'], '%Y-%m-%d %H:%M')
+    data['break'] = timedelta(minutes=int(data['break']))
+    data['vacation'] = data['vacation']=='y'
+    data['sick'] = data['sick']=='y'
+
+    return data
+
+def _db_create_convert(data):
+    ec.db.workday.create(_convert(data))
+
+def _db_modify_convert(data):
+    ec.db.workday.update_by_id(_convert(data))
+
 def _create():
-    return # TODO: Implement!
+    ec.ui.console.create(
+        {
+            'title': 'WORKDAY',
+            'entries': collections.OrderedDict([
+                ('worker_id', 'Worker ID'),
+                ('begin_at', 'Begin at'),
+                ('end_at', 'End at'),
+                ('break', 'Break'),
+                ('vacation', 'Vacation'),
+                ('sick', 'Sick')]),
+            'func': _db_create_convert
+        })
 
 def _modify():
     return # TODO: Implement!
