58b9250a21edfc95819136de7ac568158487bc58
Marc <RhinoDevel@users.noreply.github.com>
2017-05-27 17:34:07 +0200

    Improve mt.tk.table.

--- mt/tk/table.py
+++ mt/tk/table.py
@@ -1,34 +1,84 @@
 
+import logging
 import tkinter as ti
 from tkinter import ttk
 
-def _add_cell(frame, col, row, text, is_title):
-    label = ttk.Label(frame, text=text, background='red')
+border_cell = 5
+pad_table = 5
+col_bg_table = ''
+col_bg_row_deselected = ''
+col_bg_row_selected = 'yellow'
+
+def _select_row(o, r):
+    logging.debug('Selecting row at index '+str(r)+'..')
+
+    for i, row_eles in enumerate(o['ele']):
+        bg_col = col_bg_row_deselected
+        if i==r:
+            bg_col = col_bg_row_selected
+
+        for col_ele in row_eles:
+             col_ele.configure(background=bg_col)
+
+def _select_if_not(o, i, r):
+    if i!=o['sel_id']:
+        _select_row(o, r)
+        o['sel_id'] = i
+        if callable(o['on_select']):
+            o['on_select'](i)
+
+def _on_click_row(o, i, r, e):
+    logging.debug('ID of clicked row at index '+str(r)+' is "'+i+'"."')
+
+    _select_if_not(o, i, r)
+
+def _add_cell(o, col, row, i, text, is_title):
+    label = ttk.Label(o['frame'], text=text, border=border_cell)
+
+    label.grid(column=col, row=row, sticky=(ti.N, ti.S, ti.W, ti.E))
+
     if is_title:
         label.configure(font='bold')
-    label.grid(column=col, row=row, padx=5, pady=5, sticky=(ti.N, ti.W))
+    else:
+        label.bind(
+            "<Button-1>", lambda e, i=i, o=o, r=row: _on_click_row(o, i, r, e))
 
-def _add_row(frame, row, texts, are_titles):
+    o['ele'][row].append(label)
+
+    label.configure(background=col_bg_row_deselected)
+
+def _add_row(o, row, i, texts, are_titles):
     col = 0
+    o['ele'].append([])
     for text in texts:
-        _add_cell(frame, col, row, text, are_titles)
+        _add_cell(o, col, row, i, text, are_titles)
         col = col+1
 
 def create(p):
-    gui_style = ttk.Style()
-    gui_style.configure('My.TButton', foreground='#334353')
-    gui_style.configure('My.TFrame', background='#334353')
+    o = {
+        'frame': ttk.Frame(p['frame']),
+        'on_select': p['on_select'],
+        'ele': [],
+        'sel_id': ''
+    }
 
-    frame = ttk.Frame(p['frame'], style='My.TFrame')
-    frame.grid(column=0, row=0, padx=5, pady=5, sticky=(ti.N, ti.S, ti.W, ti.E))
+    o['frame'].grid(
+        column=0,
+        row=0,
+        padx=pad_table,
+        pady=pad_table,
+        sticky=(ti.N, ti.S, ti.W, ti.E))
 
     row = 0
 
-    _add_row(frame, row, p['titles'], True)
+    _add_row(o, row, None, p['titles'], True)
     row = row+1
 
-    for texts in p['entries']:
-        _add_row(frame, row, texts, False)
+    for k, v in p['entries'].items():
+        _add_row(o, row, k, v, False)
         row = row+1
 
-    return frame
+    ttk.Style().configure('MT.TFrame', background=col_bg_table)
+    o['frame'].configure(style='MT.TFrame')
+
+    return o
