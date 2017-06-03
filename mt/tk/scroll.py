3476cb51258f88943bab5b13d972ea1eec0233b4
Marc <RhinoDevel@users.noreply.github.com>
2017-06-03 18:33:50 +0200

    Scrollbar helper function (currently unused).

---
+++ mt/tk/scroll.py
@@ -0,0 +1,20 @@
+
+import tkinter as ti
+
+def _on_frame_conf(canvas):
+    canvas.configure(scrollregion=canvas.bbox('all'))
+
+def get_vertical_scrollbar_frame(frame):
+    canvas = ti.Canvas(frame)
+    inner = ti.Frame(canvas)
+    v_scrollbar = ti.Scrollbar(frame, orient='vertical', command=canvas.yview)
+
+    canvas.configure(yscrollcommand=v_scrollbar.set)
+
+    v_scrollbar.pack(side='right', fill='y')
+    canvas.pack(side='left', fill='both', expand=True)
+
+    canvas.create_window((0, 0), window=inner, anchor=ti.N+ti.W)
+    frame.bind('<Configure>', lambda e, canvas=canvas: _on_frame_conf(canvas))
+
+    return inner
