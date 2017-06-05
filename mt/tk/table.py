49f431e2c7d3087c38de3319b56c905218c95615
Marc <RhinoDevel@users.noreply.github.com>
2017-06-05 19:35:06 +0200

    Add scollbar and fix dynamic width of first cell.

--- mt/tk/table.py
+++ mt/tk/table.py
@@ -5,6 +5,7 @@ import tkinter as ti
 from tkinter import ttk
 
 import mt.tk.label
+import mt.tk.scroll
 
 class CellType(Enum):
     title = 1
@@ -13,10 +14,10 @@ class CellType(Enum):
 
 cell_text_avg_char_width = 16 # In average character width.
 cell_but_avg_char_width = 1 # In average character width.
+cell_but_pix_width = -1 # Gets initialized by _add_cell().
 border_cell = 5
 pad_table = 5
-col_bg_table = 'blue'
-col_bg_row_deselected = 'red'
+col_bg_row_deselected = ''
 col_bg_row_selected = 'yellow'
 
 # TODO: Implement scrolling.
@@ -64,33 +65,33 @@ def _on_click_row(o, i, r, e):
     _select_toggle(o, i, r)
 
 def _add_cell(o, col, row, i, text, cell_type):
+    global cell_but_pix_width
+
     is_title = cell_type==CellType.title
     is_text = cell_type==CellType.text
+    is_del = cell_type==CellType.delete
     ele = None
-    width = -1
     frame = o['frame_title'] if is_title else o['frame_data']
 
     if is_title or is_text:
-        width = cell_text_avg_char_width
         if is_title and col==0: # Delete button.
-            width = cell_but_avg_char_width
-
-        ele = ttk.Label(
-            frame,
-            text=text,
-            border=border_cell,
-            width=width)
-    else: # Assuming CellType.delete.
-        width = cell_but_avg_char_width
+            ele = ti.Frame(frame)
+        else:
+            ele = ttk.Label(
+                frame,
+                text=text,
+                border=border_cell,
+                width=cell_text_avg_char_width)
+    elif is_del:
         ele = ti.Frame(frame)
-        #ele.columnconfigure(0, weight=1)
-        #ele.rowconfigure(0, weight=1)
         but = ttk.Button(
             ele,
             text='d', # TODO: Replace with icon.
-            width=width,
+            width=cell_but_avg_char_width,
             command=lambda i=i, o=o: _on_delete(o, i))
         but.grid(column=0, row=0)
+    #
+    # Otherwise: Error!
 
     ele.grid(column=col, row=row, sticky=(ti.N, ti.S, ti.W, ti.E))
 
@@ -99,6 +100,9 @@ def _add_cell(o, col, row, i, text, cell_type):
             "<Button-1>", lambda e, i=i, o=o, r=row: _on_click_row(o, i, r, e))
         ele.update() # For next function to work.
         ele['text'] = mt.tk.label.get_ellipsis_text(ele, ele['text'])
+    elif is_del and cell_but_pix_width==-1:
+        ele.update() # For next function to work.
+        cell_but_pix_width = ele.winfo_width()
 
     ele.configure(background=col_bg_row_deselected)
 
@@ -129,6 +133,7 @@ def create(p):
     o = {
         'frame': ttk.Frame(p['frame']),
         'frame_title': None, # See below.
+        'frame_scroll': None, # See below.
         'frame_data': None, # See below.
         'on_deselect': p['on_deselect'],
         'on_select': p['on_select'],
@@ -137,7 +142,7 @@ def create(p):
         'sel_id': p['sel_id']
     }
     o['frame_title'] = ttk.Frame(o['frame'])
-    o['frame_data'] = ttk.Frame(o['frame'])
+    o['frame_scroll'] = ttk.Frame(o['frame'])
 
     o['frame'].grid(
         column=0,
@@ -145,18 +150,24 @@ def create(p):
         padx=pad_table,
         pady=pad_table,
         sticky=(ti.N, ti.S, ti.W, ti.E))
+    o['frame'].rowconfigure(1, weight=1)
+    o['frame'].columnconfigure(0, weight=1)
     o['frame_title'].grid(
         column=0,
         row=0,
         padx=0,
         pady=0,
         sticky=(ti.N, ti.S, ti.W, ti.E))
-    o['frame_data'].grid(
+    o['frame_scroll'].grid(
         column=0,
         row=1,
         padx=0,
         pady=0,
         sticky=(ti.N, ti.S, ti.W, ti.E))
+    o['frame_scroll'].rowconfigure(0, weight=1)
+    o['frame_scroll'].columnconfigure(0, weight=1)
+    o['frame_data'] = mt.tk.scroll.get_vertical_scrollbar_frame(
+        o['frame_scroll'])
 
     row = 0
     sel_row = -1
@@ -170,10 +181,9 @@ def create(p):
             sel_row = row # It would be possible to do without this..
         row = row+1
 
+    o['ele'][0][0].configure(width=cell_but_pix_width) # See _add_cell().
+
     if sel_row!=-1:
         _select_wo_call(o, o['sel_id'], sel_row)
-
-    ttk.Style().configure('MTtable.TFrame', background=col_bg_table)
-    o['frame'].configure(style='MTtable.TFrame')
 
     return o
