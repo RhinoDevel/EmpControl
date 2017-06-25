446863868f1496c4a25eeda977c6f17d6a0e6b23
Marc <RhinoDevel@users.noreply.github.com>
2017-06-25 12:02:12 +0200

    Infrastructure to let one nb_content call another's recreate function.

--- ec/ui/tk/nb_content.py
+++ ec/ui/tk/nb_content.py
@@ -17,6 +17,9 @@ def _recreate(p):
 
     mt.tk.table_input.create(p)
 
+    if 'on_recreate' in p:
+        p['on_recreate']()
+
 def _update(d, p, update):
     update(d) # TODO: Add error handling.
     _recreate(p)
@@ -34,15 +37,18 @@ def create(p):
     """Create notebook page content.
 
     Alters given parameters dictionary!
+
+    Returns altered parameters dictionary.
     """
 
     p['frame'] = ttk.Frame(p['nb'])
     p['update'] = lambda d, p = p, update = p['update']: _update(d, p, update)
     p['create'] = lambda d, p = p, create = p['create']: _create(d, p, create)
     p['delete'] = lambda i, p = p, delete = p['delete']: _delete(i, p, delete)
+    p['recreate'] = lambda p = p: _recreate(p)
 
     _recreate(p)
 
     p['nb'].add(p['frame'], text=p['title'])
 
-    return None # Add useful return value?
+    return p
