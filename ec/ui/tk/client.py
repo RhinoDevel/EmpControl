0ea3b9fe1c62440cf53f1f1ce6d0b55ceb1c8c23
Marc <RhinoDevel@users.noreply.github.com>
2017-06-25 10:54:33 +0200

    BUGFIX: Selecting created client entry.

--- ec/ui/tk/client.py
+++ ec/ui/tk/client.py
@@ -1,6 +1,4 @@
 
-# TODO: Fix BUG: 1) Create new entry. 2) Select new entry. => ERROR!
-
 import collections
 import operator
 
@@ -9,22 +7,26 @@ import ec.db.client
 import ec.db.company
 import ec.ui.tk.nb_content
 
+last_data = None
+
 def _get_sort_key(d):
     return (d['company'], d['lastname'], d['firstname'])
 
 def _read_all():
-    data = ec.db.client.read_all()
+    global last_data
+    last_data = ec.db.client.read_all()
+
     companies = ec.db.get_ordered_id_dict(ec.db.company.read_all())
 
-    for row in data:
+    for row in last_data:
         row['company'] = companies[row['company_id']]['title']
 
-    return sorted(data, key=_get_sort_key)
+    return sorted(last_data, key=_get_sort_key)
 
-def _get_val_from_id(i, d):
+def _get_val_from_id(i):
     # Probably a stupid way to do this:
     #
-    for e in d:
+    for e in last_data:
         if e['id']==i:
             return e['company_id']
 
@@ -33,10 +35,12 @@ def _get_val_from_id(i, d):
 def create(nb):
     # Get data for company select:
     #
-    data = _read_all()
+    global last_data
+    last_data = _read_all()
+    #
     company_values = []
     company_titles = []
-    for e in data:
+    for e in last_data:
         if e['company_id'] in company_values:
             continue;
         company_values.append(e['company_id'])
@@ -53,7 +57,7 @@ def create(nb):
                     'type': 'sel',
                     'values': company_values,
                     'titles': company_titles,
-                    'get_val_from_id': lambda i, d=data: _get_val_from_id(i, d)
+                    'get_val_from_id': _get_val_from_id
                 }
             ),
             ('lastname', {'title': 'Lastname', 'type': 'str'}),
